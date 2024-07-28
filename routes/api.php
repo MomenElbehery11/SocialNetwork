bbae461acb7a39acfb65e2c107a13942b8b18da4
MomenElbehery11 <141195867+MomenElbehery11@users.noreply.github.com>
2024-07-28 04:51:11 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\CommentResController;
-use App\Http\Controllers\Api\PostResController;
-
-use App\Http\Controllers\Api\LikeResController;
-
-use App\Http\Controllers\Api\UserProfileController;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -24,36 +17,3 @@ use App\Http\Controllers\Api\UserProfileController;
 Route::post('/auth/register', [UserController::class, 'createUser']);
 Route::post('/auth/login', [UserController::class, 'loginUser']);
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::resource('profiles', 'UserProfileController');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::resource('posts', 'PostController');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::resource('likes', 'LikeController');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::resource('comments', 'CommentController');
-});
-
-// Route::resource('posts', 'PostController');
